8fe2201747af3356cb4ce6209bc425ba3f50e6a7
Harshahd97 <154952682+Harshahd97@users.noreply.github.com>
2024-01-22 20:17:28 +0530

    Update sonar.groovy

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -2,3 +2,4 @@
        // Your SonarQube scan steps go here
        sh 'mvn sonar:sonar -Dsonar.projectKey=sonar-token -Dsonar.host.url=http://100.24.22.248:9000 -Dsonar.java.binaries=target/classes'
    }
+return this
